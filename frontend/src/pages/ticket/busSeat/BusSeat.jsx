212dd5056759a675dfcaa562ef8df11acafb9385
vikasprajapati-01 <vikasprajapati9980@gmail.com>
2025-05-17 23:38:24 +0530

    making route changes

--- frontend/src/pages/ticket/busSeat/BusSeat.jsx
+++ frontend/src/pages/ticket/busSeat/BusSeat.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 
-import busSeatData from "../../../constant/busSeat/busSeatData"
+import busSeatData from "../../../constant/busSeat/busSeatData.js"
 
 import { GiSteeringWheel } from "react-icons/gi";
 import { LuArmchair } from "react-icons/lu";
